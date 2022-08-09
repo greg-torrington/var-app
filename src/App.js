d3065d5406643d52f7128ae28f3726ad9a57aa27
Gregory Torrigton <gregtorrington@icloud.com>
2022-08-09 06:47:21 +0200

    Balance sorta working, allowance not.

--- src/App.js
+++ src/App.js
@@ -15,32 +15,79 @@ function App() {
     
 
   ];
-  //const useradrress = "0xa152f8bb749c55e9943a3a0a3111d18ee2b3f94e";
 
-  const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/1a737ff6f59249a1b280436c5904e123")
+  const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/1a737ff6f59249a1b280436c5904e123");
+
+  let contract = null;
+  let contractAddress = "";
+
+  const users = [];
+  let arrayLength = 0;
   
-  async function workoutApprovalAmount(){
+  async function addContract(){
 
-    const contractaddress = document.getElementById("contractAddress").value;
+    contractAddress = document.getElementById("contractAddress").value;
     //const accounts = provider.listAccounts();
-    const contract = new ethers.Contract(contractaddress, abi, provider);
+    contract = new ethers.Contract(contractAddress, abi, provider);
 
     let tokenName = await contract.name();
-    console.log("Token: " + tokenName);
+    document.getElementById('contractName').innerHTML = tokenName;
 
-    let balance = await provider.getBalance("0x3c14eE568B66Cd282EB2b987910b0e4c9650f673");
-    console.log("Balance: " + ethers.utils.formatEther(balance));
+  }
 
-    //let allowance = await contract.allowance(contractaddress, useradrress);
-    //console.log("User allowance: " + allowance);
+  async function addUser(){
+    let userAddress = document.getElementById("userAddress").value;
+    document.getElementById("userAddress").value =  "";
+    users[arrayLength] = userAddress;
 
+    let num = await contract.balanceOf(userAddress);
+    let balance = ethers.utils.formatEther(num) * 1000000000000;
+
+    let allowance = await contract.allowance(contractAddress, userAddress);
+
+    var table = document.getElementById("usersTable");
+
+    var rows = table.getElementsByTagName("tr").length;
+    var row = table.insertRow(rows);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = userAddress;
+    cell2.innerHTML = "$" + balance;
+    cell3.innerHTML = allowance;
+
+    arrayLength = arrayLength + 1;
   }
 
   return (
     <div className="App">
       <h1>VaR App</h1>
-      <input type="text" id="contractAddress"/>
-      <button onClick={() => workoutApprovalAmount()}>Submit</button>
+      <pre>
+        Contract Address: <input type="text" id="contractAddress"/>
+        <button onClick={() => addContract()}>Submit</button>
+      </pre>
+      <pre>
+        User Address: <input type="text" id="userAddress"/>
+        <button onClick={() => addUser()}>Add</button>
+      </pre>
+      <pre>
+        <button>Calculate VaR</button>
+      </pre>
+      <h2>
+        <label id="contractName"></label>
+      </h2>
+
+      <table id="usersTable" align="center">
+        <tbody>
+          <tr>
+            <th>User Address</th>
+            <th>Balance</th>
+            <th>Allowance</th>
+          </tr>
+        </tbody>
+      </table>
+
     </div>
   );
 }
